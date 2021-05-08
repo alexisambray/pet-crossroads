d3a96146254dc5c612478d5c3af094745f9f368f
Alexis Ambray <alexisambray@gmail.com>
2021-05-09 00:31:08 +0800

    Replaced mocked values in UI with live data

--- pet-crossroads-lib.h
+++ pet-crossroads-lib.h
@@ -1,9 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "constants.h"
+#include "types.h"
+
+int generateRandInt(const int lower, const int upper) {
+  return (rand() % (upper - lower + 1)) + lower;
+}
+
+int generateSundayTurnipPrice() {
+  return generateRandInt(SUNDAY_TURNIP_MIN_PRICE, SUNDAY_TURNIP_MAX_PRICE);
+}
+
+WeeklyPriceTrend generateRandTrend(void) {
+  return (WeeklyPriceTrend)generateRandInt((int)AWESOME, (int)BAD);
+}
+
+void initializeGame(int* bells,
+                    int* turnips,
+                    int* turnipPrice,
+                    int* sundayTurnipPrice,
+                    int* week,
+                    WeeklyPriceTrend* weeklyPriceTrend,
+                    DayOfWeek* dayOfWeek) {
+  srand(time(NULL));
+  *bells = STARTING_NUM_BELLS;
+  *turnips = STARTING_NUM_TURNIPS;
+  *sundayTurnipPrice = generateSundayTurnipPrice();
+  *turnipPrice = *sundayTurnipPrice;
+  *week = 1;
+  *weeklyPriceTrend = generateRandTrend();
+  *dayOfWeek = SUNDAY;
+}
+
+void displayTurnipPrice(int turnipPrice) {
+  printf("Current Turnip Price (per Turnip): %d bells\n", turnipPrice);
+}
 
 void displayWelcomeScreen(void) {
+  system("clear");
   puts(
       "***** Welcome to Pet Crossroads: Making Money off the Stalk Market "
       "*****\n");
@@ -14,24 +50,31 @@ void displayWelcomeScreen(void) {
   system("clear");
 }
 
-void displayDay(void) {
-  printf("***** Day 1 (Sunday), Week 1 of %d *****\n", MAX_WEEK);
+void displayDay(const DayOfWeek dayOfWeek, const int week) {
+  printf("***** Day %d (Sunday), Week %d of %d *****\n", dayOfWeek, week,
+         MAX_WEEK);
 }
 
-void displayResources(void) {
-  puts("5000 Bell(s)\t\t0 Turnip(s)\n");
+void displayResources(const int bells, const int turnips) {
+  printf("%d Bell(s)\t\t%d Turnip(s)\n\n", bells, turnips);
 }
 
-void displayCurrentTurnipPrice(void) {
-  puts("Current Turnip Price (per Turnip): 100 bells");
-}
-
-void displayStatus(void) {
-  displayDay();
-  displayResources();
-  displayCurrentTurnipPrice();
+void displayStatus(const DayOfWeek dayOfWeek,
+                   const int week,
+                   const int bells,
+                   const int turnips,
+                   const int turnipPrice) {
+  displayDay(dayOfWeek, week);
+  displayResources(bells, turnips);
+  displayTurnipPrice(turnipPrice);
 }
 
 void game(void) {
-  displayStatus();
+  int bells, turnips, turnipPrice, sundayTurnipPrice, week;
+  WeeklyPriceTrend weeklyPriceTrend;
+  DayOfWeek dayOfWeek;
+
+  initializeGame(&bells, &turnips, &turnipPrice, &sundayTurnipPrice, &week,
+                 &weeklyPriceTrend, &dayOfWeek);
+  displayStatus(dayOfWeek, week, bells, turnips, turnipPrice);
 }
